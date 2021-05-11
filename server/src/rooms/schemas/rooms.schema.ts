899bd06245e21a9efa072cd07e69ffaec8b9810a
Illia Hrebenko <45273513+elmorrrrro@users.noreply.github.com>
2021-05-12 02:15:36 +0300

    Added resource rooms

---
+++ server/src/rooms/schemas/rooms.schema.ts
@@ -0,0 +1,24 @@
+import * as mongoose from 'mongoose';
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { User } from 'src/user/interfaces/user.interface';
+import { v4 as uuid } from 'uuid';
+
+export type RoomDocument = Room & mongoose.Document;
+
+@Schema({ timestamps: true })
+export class Room {
+
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    topic: string;
+
+    @Prop({ required: true })
+    users: string[];
+
+    @Prop({ required: true })
+    tags: string[];
+}
+
+export const RoomSchema = SchemaFactory.createForClass(Room);
